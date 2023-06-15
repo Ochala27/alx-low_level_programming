176ffeb102122f1741f2ce385c25856a188ce287
ochala27 <ochalaonuche3@gmail.com>
2023-06-15 11:17:56 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prinf f program
+ * main - A printf f program function
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art, \n")
 return (0);
 }

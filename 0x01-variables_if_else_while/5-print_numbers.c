8b6fbff1981799276386ea3ab029aa8ffcdebd6c
ochala27 <ochalaonuche3@gmail.com>
2023-06-17 14:10:50 -0700

    Nubmer

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Number
+ * Return: always return 0
+ */
+int main(void)
+{
+int o;
+for (o = 0; o <= 9; o++)
+{
+printf("%d", o);
+}
+printf("\n");
+return (o);
+}

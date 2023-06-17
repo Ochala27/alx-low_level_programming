4033970d47810d7dae072fcd13c30b1dac67fb1c
ochala27 <ochalaonuche3@gmail.com>
2023-06-17 14:49:41 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Nubmerz
+ * Return: alway return 0
+ */
+int main(void)
+{
+int u;
+for (u = 0; u <= 0; u++)
+{
+putchar(u + 48);
+}
+putchar('\n');
+return (0);
+}

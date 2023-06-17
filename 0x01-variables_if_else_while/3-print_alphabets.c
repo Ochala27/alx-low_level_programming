06fc6f654f6b02df6b0579cf8e3d973d3b0eca0d
ochala27 <ochalaonuche3@gmail.com>
2023-06-17 12:49:26 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - alphABET
+ * Return: always return 0
+ */
+int main(void)
+{
+char n;
+char u;
+n = 'a';
+u = 'A';
+while
+(n <= 'z') {
+putchar(n);
+n++;
+}
+while
+(u <= 'Z') {
+putchar(u);
+u++;
+}
+putchar('\n');
+return (0);
+}

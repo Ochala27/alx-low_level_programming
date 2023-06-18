30406124d153b5a8a0d07532a2cf631736150ba7
ochala27 <ochalaonuche3@gmail.com>
2023-06-17 20:16:44 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Hexadecimal
+ * Return: always return 0
+ */
+int main(void)
+{
+int e;
+int l;
+for (e = 48; e <= 57; e++)
+{
+putchar(e);
+}
+for (l = 97; l <= 102; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}

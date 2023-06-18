2ca5807c0750f2399125330c4d87a8105c605c89
ochala27 <ochalaonuche3@gmail.com>
2023-06-18 12:41:12 -0700

    Two

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Two two digi numbers
+ * Return: always return 0
+ */
+int main(void)
+{
+int p, o;
+for (p = 0; p < 100; p++)
+{
+for (o = 0; o < 100; o++)
+{
+if (p < o)
+{
+putchar((p / 10) + 48);
+putchar((p % 10) + 48);
+putchar(' ');
+putchar((o / 10) + 48);
+putchar((0 % 10) + 48);
+if (p != 98 || 0 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

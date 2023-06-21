d81fc9523ee2867b9e12f8e0b2339c5bb42124f8
ochala27 <ochalaonuche3@gmail.com>
2023-06-21 06:39:19 -0700

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every mnute of the day of Jack Bauer
+ * Return: ...
+ */
+void jack_bauer(void)
+{
+int o, c, h, a;
+for (o = 0; o <= 2; o++)
+{
+for (c = 0; c <= 9; c++)
+if ((o <= 1 && c <= 9) || (o <= 2 && c <= 3))
+{
+for (h = 0; h <= 5; h++)
+{
+for (a = 0; a <= 9; a++)
+{
+_putchar(o + '0');
+_putchar(c + '0');
+_putchar(58);
+_putchar(h + '0');
+_putchar(a + '0');
+}
+}
+}
+}
+}
+

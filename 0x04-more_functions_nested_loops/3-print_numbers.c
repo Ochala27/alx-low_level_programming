cc15916da8081f04c75db5f711600a19c67d503a
ochala27 <ochalaonuche3@gmail.com>
2023-06-23 03:18:06 -0700

    number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_number - Print the numbers since 0 up to 9
+ * Return: The number since 0 up to 9
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+_putchar(c + '0');
+}
+_putchar('\n');
+}

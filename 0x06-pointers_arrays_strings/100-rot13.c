1efed2cb115d349c675b93a5692049db0d128280
ochala27 <ochalaonuche3@gmail.com>
2023-06-29 10:56:58 -0700

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int p, o;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		for (o = 0; a[o] != '\0'; o++)
+		{
+			if (s[p] == a[o])
+			{
+				s[p] = b[o];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}

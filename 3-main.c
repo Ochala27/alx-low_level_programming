e33a0fef000fcce841e88febed8bb783b905c658
ochala27 <ochalaonuche3@gmail.com>
2023-06-29 11:06:08 -0700

    rot

---
+++ 3-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printing the address of a pointer
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int *p;
+print("Adress of variable 'p': %p\n", &p);
+return (0);
+}

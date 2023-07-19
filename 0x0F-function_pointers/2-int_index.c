4f1b3fb0dba64efd824704c2eb9f165b0d0cc94b
ochala27 <ochalaonuche3@gmail.com>
2023-07-19 12:26:17 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointer.h"
+/**
+ * int_index - search for an integar
+ * @array: array to search 
+ * @size: size of the array
+ * @cmp: pointer to comparing function
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i] != 0)
+return (i);
+}
+}
+return (-1);
+}

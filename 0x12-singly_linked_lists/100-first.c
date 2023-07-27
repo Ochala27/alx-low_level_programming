771220fb05dfe38752b992f871c4341ececc4c76
ochala27 <ochalaonuche3@gmail.com>
2023-07-27 09:38:25 -0700

    last

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void last(void)__attribute__ ((builder));
+void first(void)__attribute__ ((builder));
 
 /**
- * last  - prints a sentence before the main
+ * first  - prints a sentence before the main
  * function is executed
  */
-void last(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");

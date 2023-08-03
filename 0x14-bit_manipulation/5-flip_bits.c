04e4938fb114df744eba19a2d9f484b9f027dc53
ochala27 <ochalaonuche3@gmail.com>
2023-08-03 12:48:55 -0700

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,8 @@
 /**
  * flip_bits - flip bits to convert one number to another number
  * @n: first number
- * @m: second number of bits that was needed to flip
+ * @m: second number to convert to
+ * Return: number of bits that was needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {

8fa171249cad8c0b303037f6574b2eb04022ad8c
Ochala27 <ochalaonuche3@gmail.com>
2023-10-13 09:06:35 -0700

    am back

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_dlisttint - printing a double linked list
- * @h: pointer to the head of the list
- * Return: size of the list
+ * print_dlistint - Printing a double linked list
+ * @h: Pointer to the head of the list
+ * Return: Size of the list
  */
 
 size_t print_dlistint(const dlistint_t *h)
@@ -13,7 +13,8 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		len++;
-		prntf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (len);
+}
